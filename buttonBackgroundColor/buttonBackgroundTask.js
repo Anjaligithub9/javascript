304c5a14f13196b18719aee0a5c14771fadf15b3
Anjaligithub9 <110547814+Anjaligithub9@users.noreply.github.com>
2022-09-15 16:51:24 +0530

    Add files via upload

---
+++ buttonBackgroundColor/buttonBackgroundTask.js
@@ -0,0 +1,21 @@
+const color = document.getElementById("color");
+
+red.addEventListener("click", () => {
+  color.className = " ";
+  color.classList.add("red");
+});
+
+blue.addEventListener("click", () => {
+  color.className = " ";
+  color.classList.add("blue");
+});
+
+green.addEventListener("click", () => {
+  color.className = " ";
+  color.classList.add("green");
+});
+
+black.addEventListener("click", () => {
+  color.className = " ";
+  color.classList.add("black");
+});
